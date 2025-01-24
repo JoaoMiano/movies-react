52432ad741b55dd46f485714b2a47a8f048c67d9
joao miano <joaomiano15@gmail.com>
2025-01-23 21:43:20 -0300

    create componente scrolltotop

--- src/routes/RoutesList.tsx
+++ src/routes/RoutesList.tsx
@@ -3,17 +3,19 @@ import { Home } from "../pages/Home.tsx"
 import { Movie } from '../pages/Movie.tsx'
 import { Search } from '../pages/Search.tsx'
 import App from "../App.tsx"
+import ScrollToTop from "../components/ScrollToTop.tsx"
 
-export const RoutList = ()=>{
-    return(
-        <section>
-                  <Routes>
-                    <Route element={<App />} />
-                    <Route path='/' element={<Home />} />
-                    <Route path='/movie/:id' element={<Movie />} />
-                    <Route path='/search' element={<Search />} />
-            
-                  </Routes>
-        </section>
-    )
+export const RoutList = () => {
+  return (
+    <section>
+      <ScrollToTop />
+      <Routes>
+        <Route element={<App />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/movie/:id' element={<Movie />} />
+        <Route path='/search' element={<Search />} />
+
+      </Routes>
+    </section>
+  )
 }
